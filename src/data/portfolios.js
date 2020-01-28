d58a3e265d98e0a2f8b72f7e576ed84958995351
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-01-28 23:36:12 +0530

    Add dummy portfolio data

---
+++ src/data/portfolios.js
@@ -0,0 +1,567 @@
+export default [
+  {
+    skills: [
+      'HTML5',
+      'CSS3',
+      'Grid',
+      'Flexbox',
+      'Typography',
+      'Layout',
+      'Web Design',
+      'Greyscale',
+      'Smashing Magazine',
+      'Visual',
+      'UX',
+      'UI',
+      'Design',
+      'Fonts'
+    ],
+    images: [
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/smashing1-1576522960756.png'
+    ],
+    name: 'Smashing Magazine',
+    subtitle: 'Design Teardown',
+    description:
+      'Design Teardown Project is built to understand visual hierarchies with the heatmap. In this simple project, we broke apart the website for a popular design magazine. The goal for this exercise was to train ourselves to think in terms of visual hierarchy, typography and design principles.',
+    category: 'Web Design',
+    github: 'https://github.com/KaushikShivam/smashing_magazine',
+    live:
+      'https://rawcdn.githack.com/KaushikShivam/smashing_magazine/c92981fe8f496b79836092b36f1ba27a225db6d0/index.html',
+    rating: 1
+  },
+  {
+    skills: [
+      'HTML5',
+      'CSS3',
+      'Framework',
+      '12 column',
+      'Custom Framework',
+      'Grid',
+      'Flexbox',
+      'Responsive Design',
+      'Colours',
+      'Resets',
+      'Card',
+      'Utility',
+      'Custom Scripts'
+    ],
+    images: [
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/bootgrid1-1576524130690.png'
+    ],
+    name: 'Bootgrid',
+    subtitle: 'Custom CSS Framework',
+    description:
+      'Boot grid is a lightweight 12 col grid which is packed with a lot of custom design components and features',
+    category: 'Web Design',
+    github: 'https://github.com/KaushikShivam/bootgrid',
+    live: 'https://github.com/KaushikShivam/bootgrid',
+    rating: 2
+  },
+  {
+    skills: [
+      'HTML5',
+      'CSS3',
+      'Web Design',
+      'Semantic Tags',
+      'Form',
+      'Design',
+      'Layout',
+      'Intuits'
+    ],
+    images: [
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/intuits1-1576521336307.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/intuits2-1576521336307.png'
+    ],
+    name: 'Intuits',
+    subtitle: 'Intuits clone project',
+    description:
+      'This is a Intuits clone project that I made during the Microverse Bootcamp. This project taught me how to integrate various kinds of form inputs and use of semantic tags',
+    category: 'Web Design',
+    github: 'https://github.com/rociac/forms-project',
+    live: 'https://raw.githack.com/rociac/forms-project/development/index.html',
+    rating: 3
+  },
+  {
+    skills: [
+      'HTML5',
+      'CSS3',
+      'Positioning',
+      'Backgrounds',
+      'Gradients',
+      'Flexbox',
+      'Grid'
+    ],
+    images: [
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/apple1-1576522772893.png'
+    ],
+    name: 'Apple website',
+    subtitle: 'Apple Clone Project',
+    description:
+      'This is an Apple clone of their famous landing page when they released iphone 4. I build this for my Microverse bootcamp. This project taught me positioning, flexbox, gradients, backgrounds and a lot more',
+    category: 'Web Design',
+    github: 'https://github.com/KaushikShivam/apple_clone',
+    live:
+      'https://rawcdn.githack.com/KaushikShivam/apple_clone/047b9424ce1c25b873d342e675b0a6209dbe7f74/index.html',
+    rating: 4
+  },
+  {
+    skills: [
+      'HTML5',
+      'CSS3',
+      'Embed',
+      'positioning',
+      'youtube',
+      'layout',
+      'web design',
+      'frontend'
+    ],
+    images: [
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/youtube1-1576520091309.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/youtube2-1576520091330.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/youtube3-1576520091401.png'
+    ],
+    name: 'YouTube',
+    subtitle: 'A YouTube Clone Project',
+    description:
+      'This is a youtube clone project that I made during the Microverse Bootcamp. This project taught me how to embed images and videos in an HTML5 & CSS3 Project.',
+    category: 'Web Design',
+    github: 'https://github.com/KaushikShivam/youtube_clone',
+    live:
+      'https://raw.githack.com/KaushikShivam/youtube_clone/feature-navigation/index.html',
+    rating: 5
+  },
+  {
+    skills: [
+      'HTML5',
+      'CSS3',
+      'Bootstrap',
+      'Web Design',
+      'Layout',
+      'Typography',
+      '12 Column'
+    ],
+    images: [
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/bootstrap1-1576525669253.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/bootstrap2-1576525669257.png'
+    ],
+    name: 'Bootstrap Practise',
+    subtitle: 'Bootrap MDO Template',
+    description:
+      'This is a a practise project that I forked to improve on my Bootstrap skills. I have not designed the whole thing from scratch. I have just played around with it to learn.',
+    category: 'Web Design',
+    github: 'https://github.com/KaushikShivam/bootstrap_blog_practise',
+    live:
+      'https://rawcdn.githack.com/KaushikShivam/bootstrap_blog_practise/94a174f94624b6b14143f0cd0eb56afa75ba84f3/blog.html',
+    rating: 6
+  },
+  {
+    skills: [
+      'HTML5',
+      'CSS3',
+      'Bootstrap',
+      'CSS Grids',
+      'Flexbox',
+      'Font',
+      'Landing Page',
+      'Next Week',
+      'Custom Layout',
+      'Layout',
+      'Typography',
+      'Grid',
+      '12 column'
+    ],
+    images: [
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/newsweek1-1576523766234.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/newsweek2-1576523766237.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/newsweek3-1576523766244.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/newsweek4-1576523766256.png'
+    ],
+    name: 'The Next Web',
+    subtitle: 'TNW Clone',
+    description:
+      'The Next Web Clone is a clone project that we worked on for Microverse. It uses HTML5 and CSS3. It uses CSS flexbox, grids and media queries and support all major device widths.',
+    category: 'Web Design',
+    github:
+      'https://raw.githack.com/KaushikShivam/the_next_web_clone/development/index.html',
+    live: 'https://github.com/KaushikShivam/the_next_web_clone',
+    rating: 7
+  },
+  {
+    skills: [
+      'HTML5',
+      'CSS3',
+      'positioning',
+      'floats',
+      'fixed',
+      'Web Design',
+      'grid',
+      'box model',
+      'images',
+      'semantic tags'
+    ],
+    images: [
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/nyt1-1576522501850.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/nyt2-1576522501861.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/nyt3-1576522501914.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/nyt4-1576522501928.png'
+    ],
+    name: 'New York Times',
+    subtitle: 'A NYT Clone',
+    description:
+      'This is a New York Times clone project that I made during the Microverse Bootcamp. This project taught me positioning, floats, box model, embedding images and a lot more.',
+    category: 'Web Design',
+    github: 'https://github.com/KaushikShivam/newyork-times-clone',
+    live:
+      'https://raw.githack.com/KaushikShivam/newyork-times-clone/development/index.html',
+    rating: 8
+  },
+  {
+    skills: [
+      'HTML5',
+      'CSS3',
+      'Bootstrap',
+      '12-col Grid',
+      'Web Design',
+      'Framework',
+      'CSS Grid',
+      'Flexbox',
+      'Responsive Design',
+      'Landing Page',
+      'Integrate Boostrap',
+      'Grid'
+    ],
+    images: [
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/tnw1-1576532518079.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/tnw2-1576532518084.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/tnw3-1576532518096.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/tnw4-1576532518104.png'
+    ],
+    name: 'The News Week',
+    subtitle: 'The TNW Clone',
+    description:
+      'Newsweek This is The News week page clone project that I worked on for Microverse. It uses HTML5 and CSS3, It uses CSS flexbox, 12 col grids and Bootstrap and support all major device widths.',
+    category: 'Web Design',
+    github: 'https://github.com/KaushikShivam/newsweek_clone',
+    live:
+      'https://rawcdn.githack.com/KaushikShivam/newsweek_clone/51d2d7324bedb412803bac52d7d6c6306649d6e4/index.html',
+    rating: 9
+  },
+  {
+    skills: [
+      'HTML5',
+      'CSS3',
+      'Floats',
+      'Custom Grid',
+      'Responsive Design',
+      'Flat UI',
+      'Image Embeds',
+      'Animations',
+      'Mobile Friendly',
+      'UI Design',
+      '12 Column Grid',
+      'Jquery',
+      'Media queries'
+    ],
+    images: [
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/omnifood1-1576531863406.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/omnifood2-1576531863413.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/omnifood3-1576531863426.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/omnifood4-1576531863431.png'
+    ],
+    name: 'Omnifood',
+    subtitle: 'Food Delivery App',
+    description:
+      'Omnifood - A premium landing page template for a food delivery app. Omnifood is a professional, flat and modern website template for the Restaurant delivery.',
+    category: 'Web Design',
+    github: 'https://github.com/KaushikShivam/omnifood',
+    live:
+      'https://rawcdn.githack.com/KaushikShivam/omnifood/2a4468eae6f6e0751615d347a3d48789c68b9adc/index.html',
+    rating: 10
+  },
+  {
+    skills: [
+      'HTML5',
+      'CSS3',
+      'SASS',
+      'SCSS',
+      'Floats',
+      'CSS Grids',
+      'Flexbox',
+      'Animation',
+      'Template',
+      'Task Runners',
+      'Build Scripts',
+      'Flat UI',
+      'Autoprefixer',
+      'Flat UI',
+      'Minimalism',
+      'UI Design',
+      'Responsive Design',
+      'Mobile Friendly'
+    ],
+    images: [
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/marketika1-1576531351357.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/marketika2-1576531351362.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/marketika3-1576531351371.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/marketika4-1576531351374.png'
+    ],
+    name: 'Marketika',
+    subtitle: 'We are here to help you out',
+    description:
+      'Marketika | A very professional and highly customizable sketch template with lots of custom pages and useful features. It is a custom landing page template that can be used for many domains.',
+    category: 'Web Design',
+    github: 'https://github.com/KaushikShivam/marketika',
+    live:
+      'https://rawcdn.githack.com/KaushikShivam/marketika/c7a35424e5bc25d245d6cfbf2cd72942a442e800/index.html',
+    rating: 11
+  },
+  {
+    skills: [
+      'HTML5',
+      'CSS3',
+      'SASS',
+      'SCSS',
+      'Positioning',
+      'Floating',
+      'Flexbox',
+      'CSS Grid',
+      'Animations',
+      'Flat UI',
+      'Task Runners',
+      'Build Scripts',
+      'Autoprefixer',
+      'UI Design',
+      'Flat UI',
+      'Keyframes',
+      'Landing Page',
+      'Theme',
+      'Templating',
+      'Background video',
+      'clip-path'
+    ],
+    images: [
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/natours1-1576530771982.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/natours2-1576530771988.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/natours3-1576530772002.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/natours4-1576530772007.png'
+    ],
+    name: 'Natours',
+    subtitle: 'Outdoor is where life happens',
+    description:
+      'Natours | Exciting tours for adventurous people.  Natours is a professional and modern responsive website template for the Travel industry. The template can be used for apps such as Travel blogs, Tour bookings and more.',
+    category: 'Web Design',
+    github: 'https://github.com/KaushikShivam/natours',
+    live:
+      'https://rawcdn.githack.com/KaushikShivam/natours/4689e752942b08a178655c7be225171b52959389/index.html',
+    rating: 12
+  },
+  {
+    skills: [
+      'HTML5',
+      'CSS3',
+      'SASS',
+      'SCSS',
+      'Build Scripts',
+      'Task Runner',
+      'CSS Grids',
+      'Flexbox',
+      'Mixins',
+      '7-1 Methodology',
+      'BEM',
+      'Flat UI',
+      'Minimalism',
+      'Animations',
+      'Keyframes'
+    ],
+    images: [
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/trillo1-1576527776227.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/trillo2-1576527776230.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/trillo3-1576527776241.png'
+    ],
+    name: 'Trillo',
+    subtitle: 'All in One Booking App',
+    description:
+      'Trillo is a professional, flat and modern website tempate for the Travel industry. The template can be used for apps such as Travel blogs, Hotel bookings, Flight bookings and more.',
+    category: 'Web Design',
+    github: 'https://github.com/KaushikShivam/Trillo',
+    live: 'https://raw.githack.com/KaushikShivam/Trillo/master/index.html',
+    rating: 13
+  },
+  {
+    skills: [
+      'HTML5',
+      'CSS3',
+      'SASS',
+      'SCSS',
+      'Responsive Design',
+      'Flexbox',
+      'CSS Grid',
+      'Animations',
+      'Keyframes',
+      'Task Runners',
+      'Build Scripts',
+      'Flat Design',
+      'Grid Layout',
+      'Landing Page',
+      'Template Design'
+    ],
+    images: [
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/nexter1-1576530260531.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/nexter2-1576530260533.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/nexter3-1576530260552.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/nexter4-1576530260559.png'
+    ],
+    name: 'Nexter',
+    subtitle: 'Real Estate Booking App',
+    description:
+      'Nexter is a professional, flat and modern website Real Estate tempate. It uses modern CSS features such as CSS grids and advanced animations instead of bootstrap.',
+    category: 'Web Design',
+    github: 'https://github.com/KaushikShivam/Nexter',
+    live:
+      'https://rawcdn.githack.com/KaushikShivam/Nexter/61ce36188d204dc5aec6798fd57c68a778cb7bb2/index.html',
+    rating: 14
+  },
+  {
+    skills: [
+      'HTML5',
+      'CSS3',
+      'SASS',
+      'SCSS',
+      'Animations',
+      'Keyframes',
+      'Responsive Design',
+      'BEM',
+      '7-1 Methodology',
+      'Build scripts',
+      'Task Runners',
+      'Autoprefixer',
+      'Flexbox',
+      'CSS Grids',
+      '12 Column grid'
+    ],
+    images: [
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/neoDigital1-1576528214353.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/neoDigital2-1576528214361.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/neoDigital3-1576528214371.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/neoDigital4-1576528214377.png'
+    ],
+    name: 'NeoDigital',
+    subtitle: 'Design Agency',
+    description:
+      'A very professional and highly customizable template with lots of custom pages and useful features for a Creative Marketing Agency. It uses modern design and flat UI.',
+    category: 'Web Design',
+    github: 'https://github.com/KaushikShivam/neoDigital',
+    live:
+      'https://rawcdn.githack.com/KaushikShivam/neoDigital/8ca0e4f6a19706f738fa082d3e6fde8255329214/index.html',
+    rating: 15
+  },
+  {
+    skills: [
+      'HTML5',
+      'CSS3',
+      'SASS',
+      'BEM',
+      '7-1 Structure',
+      'Animations',
+      'Flat Design',
+      'Flexbox',
+      'CSS Grid',
+      'Responsive Design',
+      'Task Runners',
+      'Build Script',
+      'AutoPrefixer',
+      'UI Design',
+      'Custom Grid',
+      'Rem',
+      'Typography'
+    ],
+    images: [
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/showtracker1-1576529385086.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/showtracker2-1576529385092.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/showtracker3-1576529385107.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/showtracker4-1576529385117.png'
+    ],
+    name: 'ShowTracker',
+    subtitle: 'Track your shows automatically',
+    description:
+      'With ShowTrackr you can track your favorite TV shows automatically, so you never loose track of your TV shows ever again.',
+    category: 'Web Design',
+    github: 'https://github.com/KaushikShivam/showTracker',
+    live:
+      'https://rawcdn.githack.com/KaushikShivam/showTracker/a963a9f01318418ab162c34ac5444850d5754c50/index.html',
+    rating: 16
+  },
+  {
+    skills: [
+      'HTML5',
+      'CSS3',
+      'Custom Grid',
+      'SASS',
+      'NPM',
+      'SCSS',
+      'Flexbox',
+      'CSS Grids',
+      '12 Column',
+      'Responsive Design',
+      'Flat UI',
+      'Bem-methodology',
+      'Modern',
+      '7-1 Pattern',
+      'Built tool',
+      'Task Runners',
+      'Animations',
+      'Keyframe'
+    ],
+    images: [
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/odigo1-1576526954678.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/odigo2-1576526954690.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/odigo3-1576526954710.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/odigo4-1576526954718.png'
+    ],
+    name: 'Odigo',
+    subtitle: 'Discover Amazing Places in Japan',
+    description:
+      'Odigo is a professional, flat and modern website Travel tempate. It uses modern CSS features such as CSS grids and advanced animations instead of bootstrap. The multi-purpose them can be used for a lot of domains in the hospitality and travel industry.',
+    category: 'Web Design',
+    github: 'https://github.com/KaushikShivam/odigo',
+    live:
+      'https://rawcdn.githack.com/KaushikShivam/odigo/9e4f8b02367a32b90f1b78ceee87061978bddfab/index.html',
+    rating: 17
+  },
+  {
+    skills: [
+      'HTML5',
+      'CSS3',
+      'Custom Grid',
+      'Flexbox',
+      'CSS Grid',
+      'Animations',
+      'SASS',
+      'BEM',
+      'Custom Grid',
+      '7-1 Architecture',
+      'Autoprefixer',
+      'Build Scripts',
+      'SCSS',
+      'Custom Grid',
+      'Custom Frameword',
+      'Responsive Design',
+      'NPM'
+    ],
+    images: [
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/castle1-1576526445237.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/castle2-1576526445244.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/castle3-1576526445277.png',
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/castle4-1576526445283.png'
+    ],
+    name: 'Castle',
+    subtitle: 'Multipurpose Landing Page',
+    description:
+      'Castle is a general purpose start up landing page template that uses a custom 12 column grid. The grid is custom built that was part of a project at Microverse. The project involved SASS, Flexbox, animations and a lot more.',
+    category: 'Web Design',
+    github: 'https://github.com/KaushikShivam/Castle-landing-page',
+    live:
+      'https://rawcdn.githack.com/KaushikShivam/Castle-landing-page/3bc8b446e45a992fc7af236495960a45eeae6de2/index.html',
+    rating: 18
+  }
+];
