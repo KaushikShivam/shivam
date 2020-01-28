072013c23af8beb3ad1c700c237c01ae5e92fdd5
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-01-29 02:42:32 +0530

    Integrate portfolio data into portfolio detail page

--- src/pages/PortfolioDetail.jsx
+++ src/pages/PortfolioDetail.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+/* eslint-disable react/jsx-no-target-blank */
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 // import PhotoGallery from '../components/PhotoGallery';
@@ -13,77 +14,88 @@ const SkillItem = ({ skill }) => {
   );
 };
 
-const PortfolioDetail = ({ portfolioItem, getPortfolio, match }) => {
+const PortfolioDetail = ({ portfolio, getPortfolio, match }) => {
   useEffect(() => {
     getPortfolio(match.params.id);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return (
-    <main className="PortfolioDetail container padding-nav">
-      {/* <PhotoGallery photos={photos} /> */}
-      <div className="PortfolioDetail__wrapper">
-        <div className="row">
-          <div className="col-lg-8">
-            <h1>BONIFACHO</h1>
-            <h4 className="info">ART COLLECTOR ECOMMERCE</h4>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum
-              voluptates ad non possimus assumenda, eveniet temporibus iusto in
-              explicabo iste excepturi voluptatibus mollitia quia soluta.
-              Doloremque cum aliquam rerum in? ssimus assumenda, eveniet
-              temporibus iusto in explicabo iste excepturi voluptatibus mollitia
-              quia soluta. Doloremque cum aliquam rerum in?
-            </p>
-          </div>
-          <div className="col-lg-4">
-            <div className="PortfolioDetail__information">
-              <table className="table">
-                <tbody>
-                  <tr>
-                    <td>Created:</td>
-                    <td>01.09.2019</td>
-                  </tr>
-                  <tr>
-                    <td>Creator:</td>
-                    <td>Shivam Kaushik</td>
-                  </tr>
-                  <tr>
-                    <td>Category:</td>
-                    <td>MERN Stack</td>
-                  </tr>
-                  <tr>
-                    <td>Visit:</td>
-                    <td>
-                      <a className="h4" href="http://www.github.com">
-                        Live Link
-                      </a>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Github:</td>
-                    <td>
-                      <a className="h4" href="http://www.github.com">
-                        Github Url
-                      </a>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+  const showPage = () => {
+    if (portfolio) {
+      const {
+        name,
+        subtitle,
+        description,
+        category,
+        skills,
+        live,
+        github
+      } = portfolio;
+
+      return (
+        <main className="PortfolioDetail container padding-nav">
+          {/* <PhotoGallery photos={photos} /> */}
+          <div className="PortfolioDetail__wrapper">
+            <div className="row">
+              <div className="col-lg-8">
+                <h1>{name}</h1>
+                <h4 className="info">{subtitle}</h4>
+                <p>{description}</p>
+              </div>
+              <div className="col-lg-4">
+                <div className="PortfolioDetail__information">
+                  <table className="table">
+                    <tbody>
+                      <tr>
+                        <td>Created:</td>
+                        <td>{new Date().getFullYear()}</td>
+                      </tr>
+                      <tr>
+                        <td>Creator:</td>
+                        <td>Shivam Kaushik</td>
+                      </tr>
+                      <tr>
+                        <td>Category:</td>
+                        <td>{category}</td>
+                      </tr>
+                      <tr>
+                        <td>Visit:</td>
+                        <td>
+                          <a className="h4" href={live} target="_blank">
+                            Live Link
+                          </a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>Github:</td>
+                        <td>
+                          <a className="h4" href={github} target="_blank">
+                            Github Url
+                          </a>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+            <div className="PortfolioDetail__skills">
+              <h4>Skills</h4>
+              <div className="content">
+                {skills.map((skill, idx) => (
+                  <SkillItem key={idx} skill={skill} />
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-        <div className="PortfolioDetail__skills">
-          <h4>Skills</h4>
-          <div className="content">
-            {/* {skills.map(skill => (
-              <SkillItem key={skill} skill={skill} />
-            ))} */}
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+        </main>
+      );
+    } else {
+      return <h1>Portfolio Item with this ID doesn't exist</h1>;
+    }
+  };
+
+  return showPage();
 };
 
 const mapStateToProps = state => ({
