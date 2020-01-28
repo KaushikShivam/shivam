7b73b1a0ca2b714ae830a199939e7ee08384d311
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-01-28 22:38:25 +0530

    Add Homepage page component

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div className="Homepage">
+      <div className="Homepage__content"></div>
+    </div>
+  );
+};
+
+export default Homepage;
