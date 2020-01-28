2c2aebca35ac5c5c63b7c65fbe8df740b9095405
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-01-28 23:38:37 +0530

    Add Work component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 // Components
 import Hero from '../layout/home/Hero';
+import Work from '../layout/home/Work';
 
 const Homepage = () => {
   return (
     <div className="Homepage">
       <div className="Homepage__content">
         <Hero />
+        <Work />
       </div>
     </div>
   );
