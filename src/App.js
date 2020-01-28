04b6c3c403aabf2a961ee2fc47df334b517b08d3
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-01-28 22:41:55 +0530

    Configure react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return <div className="App">Final</div>;
-}
+import Homepage from './pages/Homepage';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
@@ -7,4 +8,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import './styles/main.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
