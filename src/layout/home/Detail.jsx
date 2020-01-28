fedbdb34ac33d3691581cb70eafb3b3b093c89e5
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-01-29 03:07:50 +0530

    Add Detail layout component

---
+++ src/layout/home/Detail.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import DetailCard from '../../components/DetailCard';
+
+const Detail = ({ type, content }) => {
+  return (
+    <section className="grey-bg">
+      <div className="container p-sm-0">
+        <div className="row">
+          <div className="col-lg-3">
+            <h3 className="title-small">
+              <span>{type}</span>
+            </h3>
+            <p className="content-detail">{content}</p>
+          </div>
+          <div className="col-lg-9">
+            <div className="row">
+              <DetailCard />
+              <DetailCard />
+              <DetailCard />
+              <DetailCard />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Detail;
