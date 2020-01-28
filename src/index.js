60038d9e8076baedac8243faab45304c29124f32
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-01-28 22:32:20 +0530

    Add and configure bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+import './styles/main.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
