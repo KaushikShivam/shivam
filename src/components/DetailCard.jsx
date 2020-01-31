c5d9db9d8ef1bdcde2a01fd6860ac69826d23bed
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-01-31 22:57:52 +0530

    Finish integrating detailCard info

--- src/components/DetailCard.jsx
+++ src/components/DetailCard.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 
-const DetailCard = () => {
+const DetailCard = ({ item: { company, role, duration, tasks } }) => {
   return (
     <div className="DetailCard col-md-6">
       <div>
         <div className="DetailCard__wrapper">
           <div className="DetailCard__card">
-            <h2 className="DetailCard__year">2007 - 2009</h2>
-            <p className="DetailCard__job">web designer</p>
-            <p className="DetailCard__company">Corp Project</p>
+            <h2 className="DetailCard__year">{duration}</h2>
+            <p className="DetailCard__job">{role}</p>
+            <p className="DetailCard__company">{company}</p>
             <hr className="my-4" />
             <div className="DetailCard__text">
               <ul>
-                <li>Yodsmdnsmdms</li>
-                <li>Yodsmdnsmdms</li>
-                <li>Yodsmdnsmdms</li>
-                <li>Yodsmdnsmdms</li>
+                {tasks.map((task, i) => (
+                  <li key={i}>{task}</li>
+                ))}
               </ul>
             </div>
           </div>
