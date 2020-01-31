272c1bc2dba53b42a9253d9bff135729377dbe1c
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-01-31 22:15:19 +0530

    Add data work for experience

---
+++ src/data/work.js
@@ -0,0 +1,59 @@
+export default [
+  {
+    company: 'ChatBotique',
+    role: 'Founder | Full Stack Developer',
+    duration: 'Jan 2018 – Apr 2019',
+    tasks: [
+      'Built bespoke AI concierge chatbots for the luxury hotel industry from the ground up using the MERN stack',
+      'Pitched AI-driven chatbots to CIO/CTOs for international hotel chains such as Radisson Global, Emaar, Damac & Lemontree, ultimately acquiring a contract to pilot the product in one of the Radisson properties',
+      'Successfully launched the pilot before the scheduled deadline by outsourcing and managing distributed developers which reduced customer service costs by 20% and increased revenue from Food & Beverage sales'
+    ]
+  },
+  {
+    company: 'WealthGenie',
+    role: 'Founder | Full Stack Developer',
+    duration: 'Jan 2018 – Apr 2019',
+    tasks: [
+      'Produced a Lead generation chatbot for the real estate industry using the MERN stack and successfully deployed it on Facebook',
+      'Produced over 100+ hot leads for various Real Estate firms using the chatbot & Facebook marketing'
+    ]
+  },
+  {
+    company: 'Chimes Group',
+    role: 'Product Manager - SportyBay',
+    duration: 'Jul 2016 – Nov 2018',
+    tasks: [
+      'Successfully supervised and liaised with ownership, development, design and data teams for streamlined product development',
+      'Revamped the UX and UI of the product for an improved customer experience',
+      'Guided the call center & data team to procure and clean sports data for 4 major cities, comprising of 10,000 fields for actors such as coaches, academies, etc',
+      'Tackled a resource-constrained situation by undertaking the mobile development role myself to deliver the product on time'
+    ]
+  },
+  {
+    company: 'App Street Software',
+    role: 'Software Engineer (iOS)',
+    duration: 'Jun 2016 – Aug 2016',
+    tasks: [
+      'Restructured and maintained the Volkswagen app and delivered timely updates',
+      'Successfully migrated modules (ObjC to Swift) of Blackjack 42 app used by 10,000+ users'
+    ]
+  },
+  {
+    company: 'ThirstyFish',
+    role: 'Senior Mobile Developer',
+    duration: 'Jan 2016 – Jun 2016',
+    tasks: [
+      "Simultaneously developed iOS and Android versions of India's first invite-only cocktail app to minimize resource overhead",
+      'Successfully migrated modules (ObjC to Swift) of PicMe app and delivered timely updates'
+    ]
+  },
+  {
+    company: 'LightHouse',
+    role: 'Founder | iOS Developer',
+    duration: 'Oct 2014 – Dec 2015',
+    tasks: [
+      'Produced and founded a women safety mobile app with cutting edge technologies like region monitoring, real-time location tracking and sharing with multiple users, gyroscope, etc',
+      'Delivered multiple freelance projects during the same time'
+    ]
+  }
+];
