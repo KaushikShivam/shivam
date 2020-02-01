3224b9f40625a6416727a718d8b6a736f3a5eea8
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-02 03:47:43 +0530

    Finish fixing Detailcard heigh

--- src/components/DetailCard.jsx
+++ src/components/DetailCard.jsx
@@ -2,21 +2,19 @@ import React from 'react';
 
 const DetailCard = ({ item: { company, role, duration, tasks } }) => {
   return (
-    <div className="DetailCard col-12">
-      <div className="h-100">
-        <div className="DetailCard__wrapper">
-          <div className="DetailCard__card">
-            <h2 className="DetailCard__year">{role}</h2>
-            <p className="DetailCard__job">{duration}</p>
-            <p className="DetailCard__company">{company}</p>
-            <hr className="my-4" />
-            <div className="DetailCard__text">
-              <ul>
-                {tasks.map((task, i) => (
-                  <li key={i}>{task}</li>
-                ))}
-              </ul>
-            </div>
+    <div className="DetailCard col-md-6">
+      <div className="DetailCard__wrapper">
+        <div className="DetailCard__card">
+          <h2 className="DetailCard__year">{role}</h2>
+          <p className="DetailCard__job">{duration}</p>
+          <p className="DetailCard__company">{company}</p>
+          <hr className="my-4" />
+          <div className="DetailCard__text">
+            <ul>
+              {tasks.map((task, i) => (
+                <li key={i}>{task}</li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
