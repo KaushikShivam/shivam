48dcf298be7c882e28231c4991318314b312333e
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-02 03:13:52 +0530

    Add education data

---
+++ src/data/education.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    company: 'Microverse',
+    role: 'Full Stack Developer Program',
+    duration: '2018 – 2019',
+    tasks: [
+      'Learnt the core fundamentals of Full stack web development: HTML/CSS, Ruby, Ruby On Rails, React, NodeJs, Express & MongoDB, while enrolled in an extremely selective (.3% acceptance rate) full-time development program',
+      'Completed 2000+ development hours pair-programming with students from different countries while also following agile methodologies',
+      'Built 90+ projects ranging from mini projects to complex full stack applications',
+      'Volunteered to mentor 2 junior developers and helped provide guidance and help to fellow peers'
+    ]
+  },
+  {
+    company: 'Microverse',
+    role: 'Technical Support Enginner',
+    duration: 'Aug 2019 – Present',
+    tasks: [
+      'Review pull requests in HTML, Ruby & Javascript for over 30+ students',
+      'Provide specialized technical advice and guidance to multiple students over 1:1 video calls'
+    ]
+  },
+  {
+    company: 'Microverse',
+    role: 'Mentor',
+    duration: 'Sep 2019 – Present',
+    tasks: [
+      'Provide continuous support through one-to-one video conference calls and asynchronous Loom videos to mentees',
+      'Inspire and motivate junior developers to work hard',
+      'Worked with mentees to solve their personal and technical difficulties',
+      'Help prepare a structured roadmap to mentees to efficiently finish their curriculum'
+    ]
+  },
+  {
+    company: 'Better Programming',
+    role: 'Technical Writer',
+    duration: 'July 2019 – Present',
+    tasks: [
+      'Write technical articles relating to Data structures, Ruby, Javascript, and Web Design',
+      'Provide implementation support to students and junior developers'
+    ]
+  }
+];
