b54baa48174698793f1deb3c56be460f0bbef16c
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-16 12:25:46 +0530

    Add fake data

---
+++ src/data/data1.js
@@ -0,0 +1,32 @@
+const portfolios = [
+  {
+    skills: [
+      'HTML5',
+      'CSS3',
+      'Grid',
+      'Flexbox',
+      'Typography',
+      'Layout',
+      'Web Design',
+      'Greyscale',
+      'Smashing Magazine',
+      'Visual',
+      'UX',
+      'UI',
+      'Design',
+      'Fonts'
+    ],
+    images: [
+      'https://shivamkaushik.s3.ap-south-1.amazonaws.com/smashing1-1576522960756.png'
+    ],
+    name: 'Smashing Magazine',
+    subtitle: 'Design Teardown',
+    description:
+      'Design Teardown Project is built to understand visual hierarchies with the heatmap. In this simple project, we broke apart the website for a popular design magazine. The goal for this exercise was to train ourselves to think in terms of visual hierarchy, typography and design principles.',
+    category: 'Web Design',
+    github: 'https://github.com/KaushikShivam/smashing_magazine',
+    live:
+      'https://rawcdn.githack.com/KaushikShivam/smashing_magazine/c92981fe8f496b79836092b36f1ba27a225db6d0/index.html',
+    rating: 1
+  }
+];
