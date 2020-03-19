8c7f4cf428945eaac226b1892a02cc361be013b7
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-19 18:26:11 +0530

    Add Certifications data

---
+++ src/data/certifications.js
@@ -0,0 +1,55 @@
+const certifications = [
+  {
+    id: 1,
+    title: 'React',
+    info: 'Complete React Developer in 2020 (w/ Redux, Hooks, GraphQL)',
+    duration: 40,
+    image: 'https://i.ibb.co/c82MKCz/react-andrie.jpg',
+    link:
+      'https://www.udemy.com/course/complete-react-developer-zero-to-mastery/'
+  },
+  {
+    id: 2,
+    title: 'Node',
+    info: 'Node.js, Express, MongoDB & More: The Complete Bootcamp 2020',
+    duration: 42,
+    image: 'https://i.ibb.co/yns2XDz/node-jonas.jpg',
+    link: 'https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/'
+  },
+  {
+    id: 3,
+    title: 'React',
+    info: 'The Modern React Bootcamp (Hooks, Context, NextJS, Router)',
+    duration: 40,
+    image:
+      'https://i.ibb.co/Hdr0YR8/javascript.jpghttps://i.ibb.co/f9zr3CT/react-colt.jpg',
+    link: 'https://www.udemy.com/course/modern-react-bootcamp/'
+  },
+  {
+    id: 4,
+    title: 'Javascript',
+    info: 'JavaScript Bible - JavaScript Bootcamp 2020',
+    duration: 43,
+    image: 'https://i.ibb.co/Hdr0YR8/javascript.jpg',
+    link: 'https://www.udemy.com/course/javascript-bible/'
+  },
+  {
+    id: 5,
+    title: 'Ruby On Rails',
+    info: 'Dissecting Ruby on Rails 5 - Become a Professional Developer',
+    duration: 40,
+    image: 'https://i.ibb.co/26W0dt6/ror.jpg',
+    link:
+      'https://www.udemy.com/course/professional-rails-5-development-course/'
+  },
+  {
+    id: 6,
+    title: 'Ruby',
+    info: 'Learn to Code with Ruby',
+    duration: 32,
+    image: 'https://i.ibb.co/r5yRgPP/ruby.jpg',
+    link: 'https://www.udemy.com/course/learn-to-code-with-ruby-lang/'
+  }
+];
+
+export default certifications;
