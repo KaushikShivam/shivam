e3bbca2a6bf7f430ab5208abd49519ed27959f12
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-19 22:05:16 +0530

    Create blogs database

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -1,3 +1,13 @@
-const blogs = [];
+const blogs = [
+  {
+    id: 1,
+    title: 'How To Implement a Linked List Using JavaScript',
+    description:
+      'Today, I’m going to show you how to implement a singly linked list and all its common functions from scratch.',
+    link:
+      'https://medium.com/better-programming/how-to-implement-a-linked-list-using-javascript-387729416a5b',
+    image: 'https://miro.medium.com/max/1319/1*typvDGtljh_06e3Da7ciLw.png'
+  }
+];
 
 export default blogs;
