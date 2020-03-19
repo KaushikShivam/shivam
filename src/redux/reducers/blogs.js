512c7af3aba3430170ea230e0fc069fd5fcf787d
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-19 22:30:25 +0530

    Add blogsReducer

---
+++ src/redux/reducers/blogs.js
@@ -0,0 +1,12 @@
+const blogs = require('./../../data/blogs');
+
+const INITIAL_STATE = {
+  blogs
+};
+
+const blogsReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
