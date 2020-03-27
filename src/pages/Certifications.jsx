48a557163188c5aa4acbf70c18a5314e3d8a41a3
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-27 15:24:44 +0530

    Finish certifications image

--- src/pages/Certifications.jsx
+++ src/pages/Certifications.jsx
@@ -9,7 +9,7 @@ const Certifications = ({ certifications }) => {
       <Header
         title="Certifications"
         subtitle="Hours of acquired knowledge"
-        image="http://fc00.deviantart.net/fs71/i/2013/334/3/d/paperplane_wallpaper__full_hd__by_frangrgic-d6w6u49.png"
+        image="https://images.unsplash.com/photo-1523289333742-be1143f6b766?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
       />
       <div style={{ marginTop: '8rem' }} className="row">
         {certifications.map(certification => (
