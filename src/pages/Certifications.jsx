15579a0f02a7b4732eb8fefe98eaddb0608276b6
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-27 15:38:11 +0530

    Finish updating certifications

--- src/pages/Certifications.jsx
+++ src/pages/Certifications.jsx
@@ -7,7 +7,7 @@ const Certifications = ({ certifications }) => {
   return (
     <main className="container padding-nav">
       <Header
-        title="Certifications"
+        title="Training"
         subtitle="Hours of acquired knowledge"
         image="https://images.unsplash.com/photo-1523289333742-be1143f6b766?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
       />
